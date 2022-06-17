171e70a14bb3815ab5859c4b7c034d4930928f60
songmoro <97587585+songmoro@users.noreply.github.com>
2022-06-17 16:03:27 +0900

    Initial Commit

---
+++ PS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PS
+//
+//  Created by 송재훈 on 2022/06/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
